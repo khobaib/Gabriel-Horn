826cb062cbd38438a750d77f52528ac22c73f771
Touhid <tz08128@gmail.com>
2014-09-01 18:23:45 +0600

    Scan for star initialized
    ZXing library is added and its functionality is tested as OK when
    pressing the "+ scan for star" button

--- src/com/minibittechnologies/fragments/FragmentRewards.java
+++ src/com/minibittechnologies/fragments/FragmentRewards.java
@@ -4,16 +4,22 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.woodyguthriecenter.app.R;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 
-import org.woodyguthriecenter.app.R;
+import com.google.zxing.client.android.Intents;
 import com.minibittechnologies.adapter.RewardListadapter;
 import com.minibittechnologies.model.Reward;
 import com.minibittechnologies.utility.Constants;
@@ -24,141 +30,170 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
-public class FragmentRewards extends Fragment{
+public class FragmentRewards extends Fragment {
 	ListView listView;
-	ArrayList<Reward> listRewards=new ArrayList<Reward>();
+	ArrayList<Reward> listRewards = new ArrayList<Reward>();
 	RewardListadapter rewardListadapter;
+	private Button btnScanForStar;
 	int UserPoint;
-	//private List<ParseObject> tempRewardList,alredyownList;
-	//private ArrayList<ParseObject> expiredAwardList=new  ArrayList<>();
-	//private ArrayList<ParseObject> newAwardList=new  ArrayList<>();
-	private ArrayList<ParseObject> finalAwardList=new  ArrayList<>();
-	//ArrayList<String> awardObjectIdList=new ArrayList<String>();
+	// private List<ParseObject> tempRewardList,alredyownList;
+	// private ArrayList<ParseObject> expiredAwardList=new ArrayList<>();
+	// private ArrayList<ParseObject> newAwardList=new ArrayList<>();
+	private ArrayList<ParseObject> finalAwardList = new ArrayList<>();
+
+	private static final int SCANNER_REQUEST_CODE = 1111;
+
+	// ArrayList<String> awardObjectIdList=new ArrayList<String>();
 	@Override
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-		 View v=inflater.inflate(R.layout.fragment_rewards,container,false);
-		 listView=(ListView)v.findViewById(R.id.listView1);
-		 UserPoint=ParseUser.getCurrentUser().getInt(Constants.USER_REWARD_POINTS);
-		 Log.e("points",""+UserPoint);
+		View v = inflater.inflate(R.layout.fragment_rewards, container, false);
+		listView = (ListView) v.findViewById(R.id.listView1);
+		UserPoint = ParseUser.getCurrentUser().getInt(Constants.USER_REWARD_POINTS);
+		Log.e("points", "" + UserPoint);
 		// initData();
-		 getRewardList();
-		 rewardListadapter=new RewardListadapter(getActivity(),R.layout.row_list_reward, finalAwardList);
-		 listView.setAdapter(rewardListadapter);
+		getRewardList();
+		rewardListadapter = new RewardListadapter(getActivity(), R.layout.row_list_reward, finalAwardList);
+		listView.setAdapter(rewardListadapter);
 		// logOut();
-		 return v;
+		btnScanForStar = (Button) v.findViewById(R.id.btnScanForStar);
+		btnScanForStar.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				scanAndGetStar();
+			}
+		});
+		return v;
 	}
-	private void logOut()
-	{
-		ParseUser user=ParseUser.getCurrentUser();
-		 Log.e("MSG", user.getUsername());
-		  ParseUser.logOut();
-		
+
+	private void scanAndGetStar() {
+		// go to fullscreen scan
+		Intent intent = new Intent("com.touhiDroid.android.SCAN");
+		intent.putExtra(Intents.Scan.FORMATS, Intents.Scan.AZTEC_MODE + "," + Intents.Scan.PDF417_MODE + ","
+				+ Intents.Scan.QR_CODE_MODE + "," + Intents.Scan.DATA_MATRIX_MODE);
+		Log.d("SCAN_CLICK", "Starting scan");
+		startActivityForResult(intent, SCANNER_REQUEST_CODE);
 	}
-	private void getRewardList()
-	{
-		ParseQuery<ParseObject> rewardQuery=ParseQuery.getQuery(Constants.OBJECT_REWARDS);
-		rewardQuery.whereLessThanOrEqualTo(Constants.REWARD_POINTS_NEEDED,UserPoint);
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == Activity.RESULT_OK) {
+			// Handle successful scan
+			if (requestCode == SCANNER_REQUEST_CODE) {
+				// Handle scan intent
+				String contents = data.getStringExtra("SCAN_RESULT");
+				String formatName = data.getStringExtra("SCAN_RESULT_FORMAT");
+				byte[] rawBytes = data.getByteArrayExtra("SCAN_RESULT_BYTES");
+				int intentOrientation = data.getIntExtra("SCAN_RESULT_ORIENTATION", Integer.MIN_VALUE);
+				Integer orientation = (intentOrientation == Integer.MIN_VALUE) ? null : intentOrientation;
+				String errorCorrectionLevel = data.getStringExtra("SCAN_RESULT_ERROR_CORRECTION_LEVEL");
+
+				System.out.println("Contents: " + contents + "\n\n" + "Format: " + formatName + "Orientation: "
+						+ "\n\n" + orientation + "\n\n" + "Error Correction level: " + errorCorrectionLevel);
+				Log.d("SCANNED_RES", "Contents: " + contents + "\n\n" + "Format: " + formatName + "Orientation: "
+						+ "\n\n" + orientation + "\n\n" + "Error Correction level: " + errorCorrectionLevel);
+
+			}
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private void logOut() {
+		ParseUser user = ParseUser.getCurrentUser();
+		Log.e("MSG", user.getUsername());
+		ParseUser.logOut();
+
+	}
+
+	private void getRewardList() {
+		ParseQuery<ParseObject> rewardQuery = ParseQuery.getQuery(Constants.OBJECT_REWARDS);
+		rewardQuery.whereLessThanOrEqualTo(Constants.REWARD_POINTS_NEEDED, UserPoint);
 		rewardQuery.findInBackground(new FindCallback<ParseObject>() {
 
 			@Override
 			public void done(final List<ParseObject> rewardList, ParseException e) {
-				if(e==null)
-				{
-					Log.e("SIZE AwardList",""+rewardList.size());
-					for(int i=0;i<rewardList.size();i++)
-					{
-						//reward=rewardList.get(i);
-						final int pos=i;
-						ParseQuery<ParseObject> query=ParseQuery.getQuery(Constants.OBJECT_REWARDSWON);
-						
-						query.whereEqualTo(Constants.WINNER,ParseUser.getCurrentUser());
-						query.whereEqualTo(Constants.OWN_REWARD,rewardList.get(pos));
-						query.whereEqualTo(Constants.HAS_USER_CLAIMED,false);
+				if (e == null) {
+					Log.e("SIZE AwardList", "" + rewardList.size());
+					for (int i = 0; i < rewardList.size(); i++) {
+						// reward=rewardList.get(i);
+						final int pos = i;
+						ParseQuery<ParseObject> query = ParseQuery.getQuery(Constants.OBJECT_REWARDSWON);
+
+						query.whereEqualTo(Constants.WINNER, ParseUser.getCurrentUser());
+						query.whereEqualTo(Constants.OWN_REWARD, rewardList.get(pos));
+						query.whereEqualTo(Constants.HAS_USER_CLAIMED, false);
 						query.findInBackground(new FindCallback<ParseObject>() {
 
 							@Override
 							public void done(List<ParseObject> list, ParseException e) {
-								if(e==null)
-								{
-									if(list.size()==0)
-									{
-										Log.e("SIZE NOT IN", ""+list.size());
-										final ParseObject object=new ParseObject(Constants.OBJECT_REWARDSWON);
-										Date date=calculateExpiryDate(rewardList.get(pos).getLong(Constants.MILIS_BEFORE_EXPIRATION));
-										//Log.e("Time",""+reward.getLong(Constants.MILIS_BEFORE_EXPIRATION));
-										object.put(Constants.EXPIRATION_DATE,date );
-										object.put(Constants.WINNER,ParseUser.getCurrentUser());
-										object.put(Constants.OWN_REWARD,rewardList.get(pos));
-										object.put(Constants.HAS_USER_CLAIMED,false);
+								if (e == null) {
+									if (list.size() == 0) {
+										Log.e("SIZE NOT IN", "" + list.size());
+										final ParseObject object = new ParseObject(Constants.OBJECT_REWARDSWON);
+										Date date = calculateExpiryDate(rewardList.get(pos).getLong(
+												Constants.MILIS_BEFORE_EXPIRATION));
+										// Log.e("Time",""+reward.getLong(Constants.MILIS_BEFORE_EXPIRATION));
+										object.put(Constants.EXPIRATION_DATE, date);
+										object.put(Constants.WINNER, ParseUser.getCurrentUser());
+										object.put(Constants.OWN_REWARD, rewardList.get(pos));
+										object.put(Constants.HAS_USER_CLAIMED, false);
 										object.saveInBackground(new SaveCallback() {
-											
+
 											@Override
 											public void done(ParseException e) {
-												if(e==null)
-												{
-													Log.e("success","reward saved");
-													
-													ParseObject myObj=new ParseObject("Reward");
-													myObj.put("name",rewardList.get(pos).get("name"));
-													myObj.put(Constants.EXPIRATION_DATE,object.get(Constants.EXPIRATION_DATE));
+												if (e == null) {
+													Log.e("success", "reward saved");
+
+													ParseObject myObj = new ParseObject("Reward");
+													myObj.put("name", rewardList.get(pos).get("name"));
+													myObj.put(Constants.EXPIRATION_DATE,
+															object.get(Constants.EXPIRATION_DATE));
 													finalAwardList.add(myObj);
-													
+
 													rewardListadapter.notifyDataSetChanged();
+												} else {
+													Log.e("failed", e.toString());
 												}
-												else
-												{
-													Log.e("failed",e.toString());
-												}
-												
-												
+
 											}
 										});
-										
-									}
-									else if(list.size()>0)
-									{
-										for(int j=0;j<list.size();j++)
-										{
-											ParseObject myObj=new ParseObject("Reward");
-											myObj.put("name",rewardList.get(pos).get("name"));
-											myObj.put(Constants.EXPIRATION_DATE,list.get(j).get(Constants.EXPIRATION_DATE));
+
+									} else if (list.size() > 0) {
+										for (int j = 0; j < list.size(); j++) {
+											ParseObject myObj = new ParseObject("Reward");
+											myObj.put("name", rewardList.get(pos).get("name"));
+											myObj.put(Constants.EXPIRATION_DATE,
+													list.get(j).get(Constants.EXPIRATION_DATE));
 											finalAwardList.add(myObj);
 											rewardListadapter.notifyDataSetChanged();
 										}
 									}
-										
+
 								}
-								
-								
+
 							}
 						});
-						
-						
+
 					}
-					//getAvailableWonList();
-					
-					
-					
+					// getAvailableWonList();
+
 				}
-				
+
 			}
 
-			
 		});
-		
-		
+
 	}
-	private Date calculateExpiryDate(long miliLeft)
-	{
-		long curMili=System.currentTimeMillis();
-		long total=miliLeft+curMili;
-		Date date=new Date(total);
-		Log.e("date",date.toString());
+
+	private Date calculateExpiryDate(long miliLeft) {
+		long curMili = System.currentTimeMillis();
+		long total = miliLeft + curMili;
+		Date date = new Date(total);
+		Log.e("date", date.toString());
 		return date;
-		
+
 	}
-	private void initData()
-	{
+
+	private void initData() {
 		listRewards.add(new Reward("Reward 1", "7"));
 		listRewards.add(new Reward("Reward 2", "17"));
 	}
