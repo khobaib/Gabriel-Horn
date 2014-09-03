640ab66c87dcde7af4fdc10e3d028c52f04d99e4
khobaib <khobaib@gmail.com>
2014-09-03 10:51:20 +0600

    fixing compile error.

--- src/com/minibittechnologies/fragments/FragmentPostList.java
+++ src/com/minibittechnologies/fragments/FragmentPostList.java
@@ -1,7 +1,6 @@
 package com.minibittechnologies.fragments;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -22,8 +21,7 @@ import com.nineoldandroids.animation.Animator.AnimatorListener;
 import com.nineoldandroids.animation.ObjectAnimator;
 import com.parse.ParseQueryAdapter;
 
-public class FragmentPostList extends Fragment
-{
+public class FragmentPostList extends Fragment {
 	private ParseQueryAdapter<Post> postAdapter; // Adapter for the Parse query
 	private ListView lvPost;
 	private OffersTabFragment parent;
@@ -35,18 +33,15 @@ public class FragmentPostList extends Fragment
 	public static final String DEBUG_TAG = "Minibit Debug";
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-	{
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_post, container, false);
 		View topBarClone = View.inflate(getActivity(), R.layout.post_header_view, lvPost);
 
 		lvPost = (ListView) rootView.findViewById(R.id.list_view_latest_offer);
 		lvPost.addHeaderView(topBarClone);
-		lvPost.setOnItemClickListener(new OnItemClickListener()
-		{
+		lvPost.setOnItemClickListener(new OnItemClickListener() {
 			@Override
-			public void onItemClick(AdapterView<?> parent, View v, int position, long id)
-			{
+			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
 				Log.e(">>>>>>>", "pressed");
 			}
 		});
@@ -61,8 +56,7 @@ public class FragmentPostList extends Fragment
 	}
 
 	@Override
-	public void onActivityCreated(@Nullable Bundle savedInstanceState)
-	{
+	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 
 		postAdapter = new PostViewListViewAdapter(getActivity());
@@ -70,8 +64,7 @@ public class FragmentPostList extends Fragment
 	}
 
 	@Override
-	public void onResume()
-	{
+	public void onResume() {
 		super.onResume();
 		doListQuery();
 	}
@@ -79,59 +72,46 @@ public class FragmentPostList extends Fragment
 	/*
 	 * Set up a query to update the list view
 	 */
-	private void doListQuery()
-	{
-		// Refreshes the list view with new data based usually on updated location data.
+	private void doListQuery() {
+		// Refreshes the list view with new data based usually on updated
+		// location data.
 		postAdapter.loadObjects();
 	}
 
-	public void onHeaderVisibilityUpdate(boolean shouldShow)
-	{
-		if (!isTopVarVisible && shouldShow)
-		{
+	public void onHeaderVisibilityUpdate(boolean shouldShow) {
+		if (!isTopVarVisible && shouldShow) {
 			showTopBar();
-		}
-		else if (isTopVarVisible && !shouldShow)
-		{
+		} else if (isTopVarVisible && !shouldShow) {
 			hideTopBar();
 		}
 	}
 
-	public void initHeaderListener()
-	{
-		lvPost.setOnScrollListener(new OnScrollListener()
-		{
+	public void initHeaderListener() {
+		lvPost.setOnScrollListener(new OnScrollListener() {
 			private int previousScrollY = -1, currentScrollState;
 			private final int UPDATE_HEADER_THRESHOLD = 20;
 
 			@Override
-			public void onScrollStateChanged(AbsListView view, int scrollState)
-			{
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
 				currentScrollState = scrollState;
 			}
 
 			@Override
-			public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount)
-			{
+			public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
 				View firstChildView = lvPost.getChildAt(0);
-				if (firstChildView != null && currentScrollState == SCROLL_STATE_TOUCH_SCROLL)
-				{
-					int topScrollPosition = -firstChildView.getTop() + lvPost.getFirstVisiblePosition() * firstChildView.getHeight();
-					if (previousScrollY == -1)
-					{
+				if (firstChildView != null && currentScrollState == SCROLL_STATE_TOUCH_SCROLL) {
+					int topScrollPosition = -firstChildView.getTop() + lvPost.getFirstVisiblePosition()
+							* firstChildView.getHeight();
+					if (previousScrollY == -1) {
 						previousScrollY = topScrollPosition;
 					}
 
 					int dy = topScrollPosition - previousScrollY;
-					if (Math.abs(dy) > UPDATE_HEADER_THRESHOLD)
-					{
-						if (dy > 0)
-						{
+					if (Math.abs(dy) > UPDATE_HEADER_THRESHOLD) {
+						if (dy > 0) {
 							Log.e(DEBUG_TAG, "Scrolling down");
 							onHeaderVisibilityUpdate(false);
-						}
-						else
-						{
+						} else {
 							Log.e(DEBUG_TAG, "Scrolling up");
 							onHeaderVisibilityUpdate(true);
 						}
@@ -145,70 +125,62 @@ public class FragmentPostList extends Fragment
 
 	}
 
-	public void showTopBar()
-	{
+	public void showTopBar() {
 		topBar.setVisibility(View.VISIBLE);
 		ObjectAnimator showAnimator = ObjectAnimator.ofFloat(topBar, "y", -topBar.getHeight(), 0);
 		showAnimator.setDuration(HEADER_TRANSITION_DURATION);
-		showAnimator.addListener(new AnimatorListener()
-		{
+		showAnimator.addListener(new AnimatorListener() {
 			@Override
-			public void onAnimationStart(Animator arg0)
-			{}
+			public void onAnimationStart(Animator arg0) {
+			}
 
 			@Override
-			public void onAnimationRepeat(Animator arg0)
-			{}
+			public void onAnimationRepeat(Animator arg0) {
+			}
 
 			@Override
-			public void onAnimationEnd(Animator arg0)
-			{
+			public void onAnimationEnd(Animator arg0) {
 				isTopVarVisible = true;
 			}
 
 			@Override
-			public void onAnimationCancel(Animator arg0)
-			{}
+			public void onAnimationCancel(Animator arg0) {
+			}
 		});
 
 		showAnimator.start();
 	}
 
-	public void hideTopBar()
-	{
+	public void hideTopBar() {
 		ObjectAnimator hideAnimator = ObjectAnimator.ofFloat(topBar, "y", 0, -topBar.getHeight());
 		hideAnimator.setDuration(HEADER_TRANSITION_DURATION);
-		hideAnimator.addListener(new AnimatorListener()
-		{
+		hideAnimator.addListener(new AnimatorListener() {
 			@Override
-			public void onAnimationStart(Animator arg0)
-			{}
+			public void onAnimationStart(Animator arg0) {
+			}
 
 			@Override
-			public void onAnimationRepeat(Animator arg0)
-			{}
+			public void onAnimationRepeat(Animator arg0) {
+			}
 
 			@Override
-			public void onAnimationEnd(Animator arg0)
-			{
+			public void onAnimationEnd(Animator arg0) {
 				isTopVarVisible = false;
 			}
 
 			@Override
-			public void onAnimationCancel(Animator arg0)
-			{}
+			public void onAnimationCancel(Animator arg0) {
+			}
 		});
 
 		hideAnimator.start();
 	}
 
-	public OffersTabFragment getParent()
-	{
+	public OffersTabFragment getParent() {
 		return parent;
 	}
 
-	public void setParent(OffersTabFragment parent)
-	{
+	public void setParent(OffersTabFragment parent) {
 		this.parent = parent;
 	}
 }
