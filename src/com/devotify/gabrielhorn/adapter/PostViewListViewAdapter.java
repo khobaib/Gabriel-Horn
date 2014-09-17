28bd1b9b84abf6b943e8548a8d14b8ed25e0c134
khobaib <khobaib@gmail.com>
2014-09-17 13:54:52 +0600

    Post list issue fixed.

--- src/com/devotify/gabrielhorn/adapter/PostViewListViewAdapter.java
+++ src/com/devotify/gabrielhorn/adapter/PostViewListViewAdapter.java
@@ -1,30 +1,22 @@
 package com.devotify.gabrielhorn.adapter;
 
-import java.text.Format;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-
-import com.devotify.gabrielhorn.R;
-import com.devotify.gabrielhorn.activity.HolderActivity;
-import com.devotify.gabrielhorn.model.Post;
-import com.devotify.gabrielhorn.utility.Utils;
-
+import android.app.Activity;
 import android.content.Context;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.parse.GetCallback;
+import com.devotify.gabrielhorn.R;
+import com.devotify.gabrielhorn.model.Post;
+import com.devotify.gabrielhorn.utility.Utils;
 import com.parse.GetDataCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseImageView;
-import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
-import com.parse.ParseUser;
 
 public class PostViewListViewAdapter extends ParseQueryAdapter<Post> {
 	public PostViewListViewAdapter(final Context context) {
@@ -32,7 +24,7 @@ public class PostViewListViewAdapter extends ParseQueryAdapter<Post> {
 			@Override
 			public ParseQuery<Post> create() {
 				ParseQuery<Post> query = Post.getQuery();
-				query.whereEqualTo("appCompany",Utils.appCompany);	
+				query.whereEqualTo("appCompany", Utils.appCompany);
 				query.addDescendingOrder("expiration");
 				return query;
 			}
@@ -41,45 +33,57 @@ public class PostViewListViewAdapter extends ParseQueryAdapter<Post> {
 
 	@Override
 	public View getItemView(Post post, View v, ViewGroup parent) {
+
+		final ViewHolder holder;
+		LayoutInflater mInflater = (LayoutInflater) getContext().getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
 		if (v == null) {
 			v = View.inflate(getContext(), R.layout.row_post, null);
+			holder = new ViewHolder();
+			v.setTag(holder);
+		} else {
+			holder = (ViewHolder) v.getTag();
 		}
 
 		super.getItemView(post, v, parent);
 
-		ParseImageView todoImage = (ParseImageView) v.findViewById(R.id.iv_post_image);
+		holder.tvTitle = (TextView) v.findViewById(R.id.tv_title);
+		holder.tvContent = (TextView) v.findViewById(R.id.tv_content);
+		holder.ivExpired = (ImageView) v.findViewById(R.id.iv_expired);
+		holder.todoImage = (ParseImageView) v.findViewById(R.id.iv_post_image);
+
 		ParseFile imageFile = post.getParseFile("image");
 		if (imageFile != null) {
-			todoImage.setParseFile(imageFile);
-			todoImage.loadInBackground(new GetDataCallback() {
+			holder.todoImage.setParseFile(imageFile);
+			holder.todoImage.loadInBackground(new GetDataCallback() {
 				@Override
 				public void done(byte[] data, ParseException e) {
 				}
 			});
 		}
+		holder.tvTitle.setText(post.getTitle());
+		holder.tvContent.setText(post.getContent());
 
-		final TextView tvTitle = (TextView) v.findViewById(R.id.tv_title);
-		final TextView tvContent = (TextView) v.findViewById(R.id.tv_content);
-
-		tvTitle.setText(post.getTitle());
-		tvContent.setText(post.getContent());
-
-		final ImageView ivExpired = (ImageView) v.findViewById(R.id.iv_expired);
 		long currentTimeInMillis = System.currentTimeMillis();
 
 		if (post.getExpired() != null) {
 			long expiredTime = post.getExpired().getTime();
 			if (expiredTime > currentTimeInMillis) {
-				ivExpired.setVisibility(View.GONE);
+				holder.ivExpired.setVisibility(View.GONE);
 			} else {
-				ivExpired.setVisibility(View.VISIBLE);
+				holder.ivExpired.setVisibility(View.VISIBLE);
 			}
-			Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
-			String date = formatter.format(post.getExpired());
-			// Log.e(">>>>>>>", "date in Date = " + post.getExpired() +
-			// "   AND date in String = " + date);
+			// Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss",
+			// Locale.getDefault());
+			// String date = formatter.format(post.getExpired());
 		}
 
 		return v;
+	}
+
+	static class ViewHolder {
+		TextView tvTitle;
+		TextView tvContent;
+		ImageView ivExpired;
+		ParseImageView todoImage;
 	}
 }
